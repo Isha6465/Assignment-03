3067e6dd94780d35676f105877f83b6eaa44e5e5
Isha Ishtiaq <ishaishtiaq9@gmail.com>
2024-08-25 12:58:07 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center align-middle text-purple-500 m-44 font-serif font-bold text-4xl underline'><h1>Isha Ishtiaq</h1></div>
+  )
+}
+
+export default page
